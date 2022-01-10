7ba987eff26cb667ab0769d4b41c2e564728dc49
zyan <ww9720@gmail.com>
2022-01-10 17:03:20 +0900

    first commit

---
+++ src/main/java/com/salsickma/greenthumb/GreenthumbAdvancedApplication.java
@@ -0,0 +1,13 @@
+package com.salsickma.greenthumb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenthumbAdvancedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreenthumbAdvancedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/salsickma/greenthumb/GreenthumbAdvancedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salsickma.greenthumb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenthumbAdvancedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
